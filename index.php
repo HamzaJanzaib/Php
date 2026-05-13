ab60e6d8d9e597d316ec024ab3d40e08b85327f5
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-05-13 23:23:50 +0500

    add comments to explain output functionality in index.php

--- index.php
+++ index.php
@@ -4,4 +4,7 @@
 echo "Hello, World!";
 echo "<br>";
 echo "Welcome to my website.";
+
+// echo "This line is commented out and will not be executed.";
+// echo is the language construct used to output text to the browser. It can be used with or without parentheses. In this case, we are using it without parentheses to output the string "Hello, World!" and "Welcome to my website." The <br> tag is used to create a line break between the two outputs.
 ?>
