2712cd3c0735fdf7a673d90b74d6504eb87f6fb0
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-05-13 23:40:00 +0500

    add post data structure and display output in variables.php

--- variables.php
+++ variables.php
@@ -7,7 +7,13 @@ $height = 5.9;
 $isAdmin = true;
 $habits = ["reading", "coding", "traveling"];
 $data = null; // This variable is currently null
+$post = [
+    "title" => "Hello World",
+    "content" => "This is my first post.",
+    "author" => "John Doe"
+];
 
+echo "<h1 style='color: blue;'>Variables in PHP</h1>";
 
 # Displaying the variables
 echo "Name: "  . $name . "<br>";
@@ -16,4 +22,9 @@ echo "Height: " . $height . "<br>";
 echo "Is Admin: " . ($isAdmin ? "Yes" : "No") . "<br>";
 echo "Habits: " . implode(", ", $habits) . "<br>";
 echo "Data: " . ($data === null ? "null" : $data) . "<br>";
+
+echo "<hr>";
+echo "Post Title: " . $post["title"] . "<br>";
+echo "Post Content: " . $post["content"] . "<br>";
+echo "Post Author: " . $post["author"] . "<br>";
 ?>
