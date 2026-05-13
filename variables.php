c23cef50cfd39070d4b32d95791f5c64606f76f9
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-05-13 23:34:20 +0500

    add variables.php to demonstrate variable usage and output in PHP

---
+++ variables.php
@@ -0,0 +1,12 @@
+<?php
+// Variables in PHP
+// variables are used to store data and can be of different types such as strings, integers, floats, etc.
+$name = "John";
+$age = 30;
+$height = 5.9;
+
+echo "Name: "  . $name . "<br>";
+echo "Age: " . $age . "<br>";
+echo "Height: " . $height . "<br>";
+
+?>
